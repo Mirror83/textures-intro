b08d307a118d7ba445c3a898a472db87177fec2e
Mirror83 <glenochieng045@gmail.com>
2024-03-21 17:45:22 +0300

    Refactored the code into functions

--- pillow_circle/main.py
+++ pillow_circle/main.py
@@ -16,20 +16,30 @@ def image_overlay(img: Image, color=(0, 255, 0), alpha=0.2) -> Image:
     return Image.blend(bw_img, overlay, alpha)
 
 
-window = Tk()
+def crop_image(img: Image, img_mask: Image) -> Image:
+    cropped = ImageOps.fit(img, img_mask.size, centering=(0.5, 0.5))
+    cropped.putalpha(img_mask)
+    return cropped
 
-space_launch_image = Image.open("../textures/launch.bmp")
 
-mask = create_mask()
-image_tinted = image_overlay(space_launch_image)
-cropped_image: Image = ImageOps.fit(image_tinted, mask.size, centering=(0.5, 0.5))
-cropped_image.putalpha(mask)
+def main():
+    window = Tk()
 
-image_tk = ImageTk.PhotoImage(cropped_image)
+    space_launch_image = Image.open("../textures/launch.bmp")
 
-canvas = Canvas(window, width=space_launch_image.width, height=space_launch_image.height)
+    mask = create_mask()
+    image_tinted = image_overlay(space_launch_image)
+    cropped_image = crop_image(image_tinted, mask)
 
-canvas.pack()
-canvas.create_image(space_launch_image.width // 2, space_launch_image.height // 2, image=image_tk)
+    image_tk = ImageTk.PhotoImage(cropped_image)
 
-window.mainloop()
+    canvas = Canvas(window, width=space_launch_image.width, height=space_launch_image.height)
+
+    canvas.pack()
+    canvas.create_image(space_launch_image.width // 2, space_launch_image.height // 2, image=image_tk)
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
